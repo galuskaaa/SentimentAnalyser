913834b04d314de2ea604fcd49d10ad0fad443a6
AsusROG <AsusROG@ASUS>
2017-11-21 12:16:47 +0200

    Add project files.

---
+++ SentimentAnalyser/Scripts/youtupeApiCalls.js
@@ -0,0 +1,24 @@
+﻿
+
+//Comment visszaterites/
+$(document).ready(function () {
+    $("#proba").click(function (e) {
+        $.ajax({
+            dataType: "json",
+            type: 'GET',
+            url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=5&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
+            success: function (result) {
+                /*data = result;
+                $('.data').text(data);*/
+               
+                console.log(result);
+            }
+           
+        });
+     
+    });
+});
+
+
+
+
