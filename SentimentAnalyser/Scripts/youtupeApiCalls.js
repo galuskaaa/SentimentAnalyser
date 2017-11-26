6ae52f9f59bb657e381636055ba50a38d3005648
Ban Istvan <ritamaxim@gmail.com>
2017-11-26 23:25:08 +0200

    Youtube Api Commentek kinyerese egy tombe

--- SentimentAnalyser/Scripts/youtupeApiCalls.js
+++ SentimentAnalyser/Scripts/youtupeApiCalls.js
@@ -8,10 +8,17 @@ $(document).ready(function () {
             type: 'GET',
             url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=5&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
             success: function (result) {
-                /*data = result;
-                $('.data').text(data);*/
-               
+           
                 console.log(result);
+                var arrayLength = result.items.length;
+                var myStringArray=[];
+                for (var i = 0; i < arrayLength; i++)
+                {
+                    myStringArray[i] = (result.items[i].snippet.topLevelComment.snippet.textOriginal);
+                    console.log(result.items[i].snippet.topLevelComment.snippet.textOriginal);
+                }
+                    
+               
             }
            
         });
