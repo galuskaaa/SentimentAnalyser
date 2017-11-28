6b2947c84392b1171cef7130dd01eb9038ae82c7
Ban Istvan <ritamaxim@gmail.com>
2017-11-28 16:02:53 +0200

    In case of a successfull GET now i'm able to post the data to my YoutubeApiController

---
+++ SentimentAnalyser/Controllers/YoutubeApiController.cs
@@ -0,0 +1,30 @@
+﻿using SentimentAnalyser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SentimentAnalyser.Controllers
+{
+
+    public class YoutubeApiController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [System.Web.Mvc.HttpPost]
+        public JsonResult getComments(List<String> values)
+        {
+            return Json(new { Result = String.Format("Fist item in list: '{0}'", values[0]) }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+
+}
--- SentimentAnalyser/Scripts/youtupeApiCalls.js
+++ SentimentAnalyser/Scripts/youtupeApiCalls.js
@@ -1,12 +1,12 @@
 ﻿
 
 //Comment visszaterites/1
-$(document).ready(function () {
+/*$(document).ready(function () {
     $("#proba").click(function (e) {
         $.ajax({
             dataType: "json",
             type: 'GET',
-            url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=5&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
+            url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=50&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
             success: function (result) {
            
                 console.log(result);
@@ -25,6 +25,57 @@ $(document).ready(function () {
      
     });
 });
+
+*/
+
+
+//Comment visszaterites/1
+$(document).ready(function () {
+    $("#proba").click(function (e) {
+        $.ajax({
+            dataType: "json",
+            type: 'GET',
+            url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=50&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
+            success: function (result) {
+                var arrayLength = result.items.length;
+                var myStringArray = [];
+                for (var i = 0; i < arrayLength; i++) {
+                    myStringArray[i] = (result.items[i].snippet.topLevelComment.snippet.textOriginal);
+                   console.log(result.items[i].snippet.topLevelComment.snippet.textOriginal);
+                }
+                
+                console.log(typeof (myStringArray));
+                
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "YoutubeApi/getComments",
+                    datatype: "json",
+                    data: JSON.stringify(myStringArray),
+                   
+                    traditional: true,
+                    success: function (result) {
+
+                        alert("SUCCESS = " + result.d);
+                    },
+                    error: function (xmlhttprequest, textstatus, errorthrown) {
+                        alert(" conection to the server failed ");
+                        console.log("error: " + errorthrown);
+                        console.log(myStringArray);
+                    }
+                });
+
+
+            }
+
+        });
+
+    });
+});
+
+
+
+
 
 
 
