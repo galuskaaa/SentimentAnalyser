d529e3faf9f0929d048c6a8427dfc01033cd87a3
Ban Istvan <ritamaxim@gmail.com>
2017-12-05 19:04:46 +0200

    Highchart renders successfully.It displays the scorec calcuated by the vaderSharp algorithm.

--- SentimentAnalyser/Controllers/YoutubeApiController.cs
+++ SentimentAnalyser/Controllers/YoutubeApiController.cs
@@ -1,11 +1,14 @@
 ﻿using SentimentAnalyser.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using VaderSharp;
+
 
 namespace SentimentAnalyser.Controllers
 {
@@ -16,15 +19,43 @@ namespace SentimentAnalyser.Controllers
         public ActionResult Index()
         {
             return View();
+          
         }
 
 
         [System.Web.Mvc.HttpPost]
         public JsonResult getComments(List<String> values)
         {
-            return Json(new { Result = String.Format("Fist item in list: '{0}'", values[0]) }, JsonRequestBehavior.AllowGet);
+
+            double positiveScore = 0;
+            double negativeScore = 0;
+            double neutralScore = 0;
+            int commentCount = values.Count;
+            SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
+            foreach (var comment in values )
+            {
+                var results = analyzer.PolarityScores(comment);
+                positiveScore += results.Positive / commentCount;
+                negativeScore += results.Negative / commentCount;
+                neutralScore += results.Compound / commentCount;
+            }
+            //Debug.WriteLine("Analysis score||||||||||||||||||||||||||||",(score).ToString());
+            //return Json(new { Result = String.Format("Analysis score:",values.Count) });
+            //return new EmptyResult();
+
+            
+                var data = new[]
+                {
+                  new { name = "Positive", y = positiveScore },
+                  new { name = "Negative", y = negativeScore },
+                  new { name = "Neutral" , y = neutralScore }
+                
+                };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+
         }
-
     }
-
 }
+
+
--- SentimentAnalyser/Scripts/youtupeApiCalls.js
+++ SentimentAnalyser/Scripts/youtupeApiCalls.js
@@ -1,35 +1,96 @@
 ﻿
 
-//Comment visszaterites/1
+
+//Comment lekerese,success eseten POST a controllerhez.
 /*$(document).ready(function () {
     $("#proba").click(function (e) {
         $.ajax({
             dataType: "json",
             type: 'GET',
             url: "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=50&videoId=lyZQPjUT5B4&fields=items%2Fsnippet%2FtopLevelComment%2Fsnippet%2FtextOriginal&key=AIzaSyBb1hVnsuI_8HLkANAt7CCPUmBiygPzAnE",
-            success: function (result) {
-           
-                console.log(result);
+            success: function (result)
+            {
                 var arrayLength = result.items.length;
-                var myStringArray=[];
+                var myStringArray = [];
                 for (var i = 0; i < arrayLength; i++)
                 {
                     myStringArray[i] = (result.items[i].snippet.topLevelComment.snippet.textOriginal);
-                    console.log(result.items[i].snippet.topLevelComment.snippet.textOriginal);
                 }
-                    
-               
+                
+                
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "YoutubeApi/getComments",
+                    datatype: "json",
+                    data: JSON.stringify(myStringArray),
+                    traditional: true,
+                    success: function (result)
+                    {
+
+                        alert("SUCCESS = " + result.d);
+                    },
+                    error: function (xmlhttprequest, textstatus, errorthrown)
+                    {
+                        alert(" conection to the server failed ");
+                        console.log("error: " + errorthrown);
+                        console.log(myStringArray);
+                    }
+                });
+
+
             }
-           
+
         });
-     
+
     });
 });
-
 */
 
+//HighChart felepitese.A sikeres POST kuldes eseten,a controllerben megjelenik egy Lista amely tartalmazza a commenteket,majd az erzelemtesztelo
+//kiertekeli ezeket es egy JSON objektumot kuld vissza.Ha ez sikeres,akkor felepitem a Highchartot a visszakuldott adatbol.
 
-//Comment visszaterites/1
+function sentimentAnalysisData(data)
+    //$(document).ready(function ()
+{
+    var chart = new Highcharts.Chart({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            renderTo: 'highchart'
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: 'Sentiment Analysis'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            data: data
+        }]
+           
+        //});
+    })
+};
+
 $(document).ready(function () {
     $("#proba").click(function (e) {
         $.ajax({
@@ -41,21 +102,18 @@ $(document).ready(function () {
                 var myStringArray = [];
                 for (var i = 0; i < arrayLength; i++) {
                     myStringArray[i] = (result.items[i].snippet.topLevelComment.snippet.textOriginal);
-                   console.log(result.items[i].snippet.topLevelComment.snippet.textOriginal);
                 }
-                
-                console.log(typeof (myStringArray));
-                
+
+
                 $.ajax({
                     type: "POST",
                     contentType: "application/json; charset=utf-8",
                     url: "YoutubeApi/getComments",
                     datatype: "json",
                     data: JSON.stringify(myStringArray),
-                   
                     traditional: true,
                     success: function (result) {
-
+                        sentimentAnalysisData(result);
                         alert("SUCCESS = " + result.d);
                     },
                     error: function (xmlhttprequest, textstatus, errorthrown) {
@@ -72,8 +130,6 @@ $(document).ready(function () {
 
     });
 });
-
-
 
 
 
