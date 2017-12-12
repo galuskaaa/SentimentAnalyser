7fe32ea336b6f0e4a90ce0b257342fc012814e17
Ban Istvan <ritamaxim@gmail.com>
2017-12-12 10:22:42 +0200

    Comment analysis is happening now in another location.(Refactor 0.0.1).Exceptions still not handled

--- SentimentAnalyser/Controllers/YoutubeApiController.cs
+++ SentimentAnalyser/Controllers/YoutubeApiController.cs
@@ -7,8 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
-
+using SentimentAnalyser.Helper;
 
 namespace SentimentAnalyser.Controllers
 {
@@ -27,30 +26,8 @@ namespace SentimentAnalyser.Controllers
         public JsonResult getComments(List<String> values)
         {
 
-            double positiveScore = 0;
-            double negativeScore = 0;
-            double neutralScore = 0;
-            int commentCount = values.Count;
-            SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
-
-            foreach (var comment in values )
-            {
-                var results = analyzer.PolarityScores(comment);
-                positiveScore += results.Positive / commentCount;
-                negativeScore += results.Negative / commentCount;
-                neutralScore += results.Compound / commentCount;
-            }
-
-                var data = new[]
-                {
-                  new { name = "Positive", y = positiveScore },
-                  new { name = "Negative", y = negativeScore },
-                  new { name = "Neutral" , y = neutralScore }
-                
-                };
-
-        
-            return Json(data, JsonRequestBehavior.AllowGet);
+            CommentAnalysisAction commentAnalysisAction = new CommentAnalysisAction();
+            return Json(commentAnalysisAction.evaluateCommentSentiment(values), JsonRequestBehavior.AllowGet);
 
         }
     }
---
+++ SentimentAnalyser/Helper/CommentAnalysisAction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VaderSharp;
+
+namespace SentimentAnalyser.Helper
+{
+    public class CommentAnalysisAction
+    {
+        public  CommentAnalysisAction()
+        {
+
+        }
+
+
+        public Object[] evaluateCommentSentiment(List<String> values)
+        {
+            
+            double positiveScore = 0;
+            double negativeScore = 0;
+            double neutralScore = 0;
+            int commentCount = values.Count;
+            SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
+
+            foreach (var comment in values)
+            {
+                var results = analyzer.PolarityScores(comment);
+                positiveScore += results.Positive / commentCount;
+                negativeScore += results.Negative / commentCount;
+                neutralScore += results.Compound / commentCount;
+            }
+
+            var data = new[]
+            {
+                  new { name = "Positive", y = positiveScore },
+                  new { name = "Negative", y = negativeScore },
+                  new { name = "Neutral" , y = neutralScore }
+
+            };
+
+            return data;
+        }
+
+
+
+    }
+
+
+
+}
